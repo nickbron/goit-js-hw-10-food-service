53e672368df15554f1fc5a8c1af169447aa2064a
nickbron <77615583+nickbron@users.noreply.github.com>
2021-06-21 13:11:13 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector("body");
+const themeswitchRef = document.querySelector("#theme-switch-toggle");
+localStorage.setItem('Theme', 'LIGHT');
+
+themeswitchRef.addEventListener("change", onChangeThemes);
+
+function onChangeThemes()
+{
+    localStorage.setItem('Theme', 'DARK');
+    bodyRef.classList.toggle(theme);
+}
+
