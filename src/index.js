62e515ba349bceae9ffef2f9ebfa69044d7b303a
nickbron <77615583+nickbron@users.noreply.github.com>
2021-06-23 12:48:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './sass/main.scss';
+import menuCards from './templates/menu-card.hbs';
+import menu from './menu.json';
+
+const menuListRef = document.querySelector('.js-menu');
+menuListRef.insertAdjacentHTML('beforeend', menuCards(menu));
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -7,13 +12,29 @@ const Theme = {
 
 const bodyRef = document.querySelector("body");
 const themeswitchRef = document.querySelector("#theme-switch-toggle");
-localStorage.setItem('Theme', 'LIGHT');
-
+console.log(menuCards);
 themeswitchRef.addEventListener("change", onChangeThemes);
+getThemeFromLocalStorage();
 
 function onChangeThemes()
-{
-    localStorage.setItem('Theme', 'DARK');
-    bodyRef.classList.toggle(theme);
+{   //Удаление класса
+    for (const el of Object.values(Theme)) {
+    if (bodyRef.classList.contains(el)) {
+        bodyRef.classList.remove(el);
+    }
+  }
+    const activeTheme = themeswitchRef.checked ? Theme.DARK : Theme.LIGHT;
+    bodyRef.classList.add(activeTheme);
+    localStorage.setItem('Theme', activeTheme);
 }
+
+function getThemeFromLocalStorage ()  {
+  const localStorageTheme = localStorage.getItem('Theme');
+
+    if (localStorageTheme === Theme.DARK) {
+     themeswitchRef.checked = true;
+     onChangeThemes();
+  }
+};
+
 
