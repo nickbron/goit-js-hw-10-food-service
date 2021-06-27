58c2b6f0dce7ef9052b6f01c6ed7ea2071b4f601
nickbron <77615583+nickbron@users.noreply.github.com>
2021-06-27 12:17:28 +0300

    update

--- src/index.js
+++ src/index.js
@@ -12,28 +12,31 @@ const Theme = {
 
 const bodyRef = document.querySelector("body");
 const themeswitchRef = document.querySelector("#theme-switch-toggle");
-console.log(menuCards);
+
 themeswitchRef.addEventListener("change", onChangeThemes);
+
 getThemeFromLocalStorage();
 
 function onChangeThemes()
-{   //Удаление класса
-    for (const el of Object.values(Theme)) {
+{   //Удалеем класс
+  for (const el of Object.values(Theme))
+  {
     if (bodyRef.classList.contains(el)) {
-        bodyRef.classList.remove(el);
+      bodyRef.classList.remove(el);
     }
   }
-    const activeTheme = themeswitchRef.checked ? Theme.DARK : Theme.LIGHT;
-    bodyRef.classList.add(activeTheme);
-    localStorage.setItem('Theme', activeTheme);
+
+  const activeTheme = themeswitchRef.checked ? Theme.DARK : Theme.LIGHT;
+  bodyRef.classList.add(activeTheme);
+  localStorage.setItem('Theme', activeTheme);
 }
 
 function getThemeFromLocalStorage ()  {
   const localStorageTheme = localStorage.getItem('Theme');
 
-    if (localStorageTheme === Theme.DARK) {
-     themeswitchRef.checked = true;
-     onChangeThemes();
+  if (localStorageTheme === Theme.DARK) {
+    themeswitchRef.checked = true;
+    onChangeThemes();
   }
 };
 
